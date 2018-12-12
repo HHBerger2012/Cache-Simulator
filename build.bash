0f16be1bd41bbdbf09fc8d4d1de4b9b8970e459c
Henry Berger <henryhaddonberger@gmail.com>
2018-12-12 11:59:43 -0800

    Update build.bash

--- build.bash
+++ build.bash
@@ -5,7 +5,7 @@ $compile -o objs/command_line.o command_line.c
 
 gcc -shared -fPIC -o libname.so objs/command_line.o
 
-gcc -L/home/aplaza/hwk_8/ -std=c99 -Wall -g \
+gcc -L/Users/henryberger/Desktop/Shell-in-C-master/ -std=c99 -Wall -g \
     -o shell_main shell_main.c\
     -lname
 
